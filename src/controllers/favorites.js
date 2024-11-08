7a133c721d53be13f09724abecefc97e6723fb20
= <=>
2024-11-07 23:42:45 -0300

    working on crud favorites

--- src/controllers/favorites.js
+++ src/controllers/favorites.js
@@ -2,25 +2,32 @@ const {
   connectToCollection,
   disconnectFromMongo,
 } = require('../database/config');
+const { ObjectId } = require('mongodb');
 const remove_id = require('../utils/remove_id');
 
 const pushProducts = async (req, res) => {
   try {
     /* 672ac1c5269e19ede8d4d940 */
-    const userId = req.params;
+    const { userId } = req.query;
     console.log('id param', userId);
     console.log('user', req.user);
-
+    console.log('body', req.body);
     const product = req.body;
 
     const users = await connectToCollection('users');
-    const userUpdateFavorites = await users.updateOne(userId, {
-      $push: { favorites: product },
-    });
-    console.log(userUpdateFavorites);
+    const userUpdateFavs = await users.findOneAndUpdate(
+      {
+        _id: new ObjectId(userId),
+      },
+      { $push: { favorites: req.body } }
+    );
+
+    console.log(userUpdateFavs);
 
     return res.status(200).json({ ok: true, message: 'push' });
   } catch (error) {
+    console.log(error);
+
     return res.status(500).json({ ok: false, message: 'server error' });
   }
 };
